eaa82f7ff43bd27ea50cf5c4251f5a53614169e8
VeselinovStf <chofexx@gmail.com>
2019-01-18 11:13:39 +0200

    Initial Commit -> All basic templates at start point with migrated ( added ASP Identity User ) database. All functionality is separated in 3-layer architecture. Namespaces and Naming comvention is done.

---
+++ CI.Data/Identity/CoreIdentityTemplateIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CI.Data.Identity
+{
+    // Add profile data for application users by adding properties to the CIWebUser class
+    public class CoreIdentityTemplateIdentityUser : IdentityUser
+    {
+    }
+}
